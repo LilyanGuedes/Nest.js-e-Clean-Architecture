54739e89ffaee47357aad88b03d5d9cb15cb6286
LilyanGuedes <108057303+LilyanGuedes@users.noreply.github.com>
2024-05-05 22:13:33 -0300

    use-case start

---
+++ src/projects/use-cases/start-project.use-case.ts
@@ -0,0 +1,17 @@
+import { StartProjectDto } from "../dto/start-project.dto";
+import { Inject } from "@nestjs/common";
+import { IProjectRepository } from "../project.repository";
+
+export class StartProjectUseCase {
+  constructor( 
+    @Inject('IProjectRepository')
+    private readonly projectRepo: IProjectRepository
+  ) {}
+
+  async execute(id: string, input: StartProjectDto){
+    const project = await this.projectRepo.findById(id);
+    project.start(input.started_at);
+    await this.projectRepo.update(project);
+    return project;
+  }
+}
